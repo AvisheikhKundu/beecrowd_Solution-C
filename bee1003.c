0dfdd320b32a9ca89c2d0bbd9cc4992a3367eaa0
Avisheikh Kundu <avisheikh76@gmail.com>
2023-10-09 14:37:55 +0600

    update all

---
+++ bee1003.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+    int main()
+    {
+         int x,y;
+      
+         scanf("%d %d", &x, &y);
+         scanf("%d", &y);
+         printf("SOMA = %d\n", x + y);
+        return 0;
+    }              
---
+++ bee1013.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+    int main()
+    {
+         int x, y, z, tmp;
+         scanf("%d %d %d", &x, &y, &z);
+         tmp = x;
+         if(x < y || x < z){
+             if(y < z){
+             tmp = z;
+             }
+         else{
+               tmp = y;
+            }
+        }
+        printf("%d eh o maior\n", tmp);
+        return 0;
+    }              
